ee962751922b00f1cdefdf0a53352b70187cee48
Sean Tay <seantay@Seans-MacBook-Pro-2.local>
2016-06-29 19:05:02 -0400

    Working Post

---
+++ db/migrate/20160629003646_comments.rb
@@ -0,0 +1,9 @@
+class Comments < ActiveRecord::Migration
+  def change
+    create_table :comments do |t|
+      t.string :body
+      t.references :post, index: true, foreign_key: true
+      t.timestamps null: false
+    end
+  end
+end
